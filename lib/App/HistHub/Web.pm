4725bf83fab232cb1720faf7d3337254ed3b061a
Daisuke Murase <typester@cpan.org>
2008-11-12 11:53:11 +0900

    init api

--- lib/App/HistHub/Web.pm
+++ lib/App/HistHub/Web.pm
@@ -1,38 +1,19 @@
 package App::HistHub::Web;
-
 use strict;
 use warnings;
 
 use Catalyst::Runtime '5.70';
-
-# Set flags and add plugins for the application
-#
-#         -Debug: activates the debug mode for very useful log messages
-#   ConfigLoader: will load the configuration from a Config::General file in the
-#                 application's home directory
-# Static::Simple: will serve static files from the application's root 
-#                 directory
-
 use parent qw/Catalyst/;
-use Catalyst qw/-Debug
-                ConfigLoader
-                Static::Simple/;
-our $VERSION = '0.01';
 
-# Configure the application. 
-#
-# Note that settings in app_histhub_web.conf (or other external
-# configuration file that you set up manually) take precedence
-# over this when using ConfigLoader. Thus configuration
-# details given here can function as a default configuration,
-# with a external configuration file acting as an override for
-# local deployment.
+use Catalyst qw/
+    ConfigLoader
+    /;
 
-__PACKAGE__->config( name => 'App::HistHub::Web' );
+__PACKAGE__->config(
+    
+);
 
-# Start the application
-__PACKAGE__->setup();
-
+__PACKAGE__->setup;
 
 =head1 NAME
 
---
+++ lib/App/HistHub/Web/Controller/API.pm
@@ -0,0 +1,72 @@
+package App::HistHub::Web::Controller::API;
+use strict;
+use warnings;
+use parent 'Catalyst::Controller';
+
+__PACKAGE__->mk_accessors(qw/result error/);
+
+use Data::UUID;
+use Digest::SHA1 qw/sha1_hex/;
+
+=head1 NAME
+
+App::HistHub::Web::Controller::API - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=head2 init
+
+=cut
+
+sub init :Local :Args(0) {
+    my ($self, $c) = @_;
+
+    my $peer = $c->model('DB::Peer')->create({});
+    $self->result({ uid => $peer->uid });
+}
+
+=head2 end
+
+=cut
+
+sub end :Private {
+    my ($self, $c) = @_;
+
+    if (@{ $c->error }) {
+        (my $last_error = $c->error->[-1]) =~ s/ on .*?$//;
+
+        $c->res->status(500);
+        $c->stash->{json} = {
+            error => $last_error,
+        };
+        $c->error(0);
+    }
+    elsif ($self->result or $self->error) {
+        $c->stash->{json} = {
+            result => $self->result || '',
+            error  => $self->error || '',
+        };
+    }
+
+    use YAML::Syck;
+    warn Dump $c->stash;
+
+    $c->forward( $c->view('JSON') );
+}
+
+=head1 AUTHOR
+
+Daisuke Murase
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
